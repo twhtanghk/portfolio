ff42ababd7a1d83c77b87cb2102b65546a530e92
root <twhtanghk@gmail.com>
2018-03-27 12:21:31 +0800

    add tags multiselect control

---
+++ frontend/src/tags.vue
@@ -0,0 +1,14 @@
+<template>
+  <mselect v-model='list' :options='list' :multiple='true' />
+</template>
+
+<script lang='coffee'>
+require 'vue-multiselect/dist/vue-multiselect.min.css'
+
+module.exports =
+  components:
+    mselect: require('vue-multiselect').default
+  props: [
+    'list'
+  ]
+</script>
