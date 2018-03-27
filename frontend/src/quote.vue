3335da9a122abdbdd3e5c7f4481771d0b9e4de4b
root <twhtanghk@gmail.com>
2018-03-27 12:20:56 +0800

    add quote hyperlink for specified symbol

---
+++ frontend/src/quote.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :href='url' target='_blank'>{{symbol}}</a>
+</template>
+
+<script lang='coffee'>
+module.exports =
+  props: [
+    'symbol'
+  ]
+  computed:
+    url: ->
+      "https://hk.finance.yahoo.com/quote/#{@symbol}.HK"
+</script>
