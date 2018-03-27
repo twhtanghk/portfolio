8fd00cae23191b5743d48637242ee8b0a50ce625
root <twhtanghk@gmail.com>
2018-03-27 12:17:35 +0800

    add multiselect control

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <authForm :eventBus='eventBus' :oauth2='oauth2' />
-    <model ref='portfolio' :eventBus='eventBus' baseUrl='http://172.22.0.3:1337/api/portfolio' />
+    <model ref='portfolio' :eventBus='eventBus' baseUrl='http://172.23.0.3:1337/api/portfolio' />
+    <model ref='onhold' :eventBus='eventBus' baseUrl='http://172.23.0.3:1337/api/portfolio/onhold' />
     <b-container>
       <b-row>
         <b-col>
-          <portfolio :collection='collection' />
+          <tags :list='tagsData' />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <onhold :collection='onhold' />
         </b-col>
         <b-col>
-          <onhold :collection='collection' />
+          <portfolio :collection='portfolio' />
         </b-col>
       </b-row>
     </b-container>
@@ -33,6 +39,7 @@ module.exports =
   components:
     portfolio: require('./portfolio').default
     onhold: require('./onhold').default
+    tags: require('./tags').default
   data: ->
     oauth2:
       url: 'https://app.ogcio.gov.hk/auth/oauth2/authorize/'
@@ -40,8 +47,13 @@ module.exports =
       scope: 'User'
       response_type: 'token'
     eventBus: eventBus
-    collection: []
+    portfolio: []
+    onhold: []
+    tagsData: []
   mounted: ->
+    @$refs.portfolio.read 'tags'
+      .then (res) =>
+        @tagsData = res
     gen = await @$refs.portfolio.listAll
       data:
         type:
@@ -55,8 +67,12 @@ module.exports =
     do =>
       {next} = gen()
       while true
-        {done, value} = await next @collection.length
+        {done, value} = await next @portfolio.length
         break if done
         for i in value
-          @collection.push i
+          @portfolio.push i
+    @$refs.onhold.list()
+      .then (res) =>
+        for i in res
+          @onhold.push i
 </script>
