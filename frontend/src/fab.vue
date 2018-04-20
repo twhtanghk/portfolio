6dcaeed17b0bb33c6ac399ea2b1fdbb9540096ae
root <twhtanghk@gmail.com>
2018-04-20 10:35:21 +0800

    add customized fab

---
+++ frontend/src/fab.vue
@@ -0,0 +1,15 @@
+<template>
+  <fab :actions='actions' bg-color='#007bff' />
+</template>
+
+<script lang='coffee'>
+upload = require('vue-fab/src/upload').default
+
+module.exports =
+  components:
+    fab: require('vue-fab').default
+  data: ->
+    actions: [
+      { name: 'upload', tooltip: 'Upload', 'icon-component': upload }
+    ]
+</script>
