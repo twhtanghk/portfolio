7e3a435542c24992e94362f6fb7676e1e2f2826e
root <twhtanghk@gmail.com>
2018-04-26 10:46:26 +0800

    remove unused create form component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <fab :eventBus='eventBus' />
-    <create :eventBus='eventBus' />
     <authForm :eventBus='eventBus' :oauth2='oauth2' />
     <model ref='portfolio' :eventBus='eventBus' baseUrl='api/portfolio' />
     <b-container>
@@ -35,7 +34,6 @@ module.exports =
     hold: require('./hold').default
     mselect: require('vue-multiselect').default
     fab: require('./fab').default
-    create: require('./portfolioForm').default
   data: ->
     oauth2:
       url: process.env.AUTH_URL
