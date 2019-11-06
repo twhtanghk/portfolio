c69c184355cbf3b92def2f630603f69c3ff4befe
root <twhtanghk@gmail.com>
2019-11-06 21:14:08 +0800

    extend hsi to sector advance decline line

---
+++ frontend/src/sector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ ad }}</div>
+</template>
+
+<script lang='coffee'>
+{Sector} = require('./model').default
+
+export default
+  data: ->
+    ad: {}
+  created: ->
+    sectors = [
+      'HSI'
+      'material'
+      'communication'
+      'consumerDiscretionary'
+      'consumerStaples'
+      'energy'
+      'financial'
+      'healthcare'
+      'industrial'
+      'IT'
+    ]
+    for sector in sectors
+      @ad = _.extend @ad, 
+        sector: JSON.stringify await Sector.ad data: sector: sector
+</script>
