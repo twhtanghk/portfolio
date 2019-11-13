76c71676541dbcbe3274ac170472281d6a1df730
root <twhtanghk@gmail.com>
2019-11-13 16:53:51 +0800

    derive stop loss price from 1 month high instead of current price

--- frontend/src/mqtt.vue
+++ frontend/src/mqtt.vue
@@ -10,7 +10,7 @@ client.apply = (list) ->
     mergeQuote = (item, msg) ->
       item.quote = Object.assign item.quote, msg.quote
       item.currTotal = item.quote.curr * item.quantity
-      item.stopLoss = parseFloat(process.env.STOPLOSS) * Math.max item.price, item.quote.curr
+      item.stopLoss = parseFloat(process.env.STOPLOSS) * Math.max item.price, msg.history['1個月'].high
     for item in list
       if msg.symbol == parseInt item.symbol
         switch msg.src
@@ -19,6 +19,7 @@ client.apply = (list) ->
           when 'aastocks'
             mergeQuote item, msg
             item.details = Object.assign item.details, msg.details
+            item['history'] = msg.history
             if item.details.dividend[3]?
               item.details.dividend[3] = new Date item.details.dividend[3]
             item.name = msg.name
