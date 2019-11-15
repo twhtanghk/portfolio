bbc37bdec43f4734e2956a090f7afef477658e46
root <twhtanghk@gmail.com>
2019-11-15 12:21:22 +0800

    replace existing table with vuetify data table

--- frontend/src/tx.vue
+++ frontend/src/tx.vue
@@ -3,68 +3,26 @@
     <v-btn color='primary' fixed fab bottom right @click='showAdd = true'>
       <v-icon>add</v-icon>
     </v-btn>
-    <v-layout column v-scroll='nextPage'>
-      <v-flex xs12>
-        <row class='portfolio-header'>
-          <template v-slot:col1>
-            <div>Name</div>
-            <div>Symbol</div>
-          </template>
-          <template v-slot:col2>
-            <div>Quantity</div>
-            <div>Type / Price / Date</div>
-          </template>
-          <template v-slot:col3>
-            <div>Tag</div>
-            <div>Notes</div>
-          </template>
-          <template v-slot:col4>
-            <div>Total</div>
-            <div>P&ampL</div>
-          </template>
-        </row>
-        <row v-if='showAdd'>
-          <template v-slot:col1>
-            <v-text-field v-model='tx.type' label='Type' />
-            <v-text-field v-model='tx.symbol' label='Symbol' />
-          </template>
-          <template v-slot:col2>
-            <v-text-field v-model='tx.quantity' label='Quantity' type='number'/>
-            <v-text-field v-model='tx.price' label='Price' type='number' />
-          </template>
-          <template v-slot:col3>
-            <v-menu
-              v-model='menu'
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-              min-width="290px">
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  v-model="tx.date"
-                  prepend-icon="event"
-                  label='Date'
-                  readonly
-                  v-on="on" />
-              </template>
-              <v-date-picker v-model="tx.date" no-title @input="menu = false"/>
-            </v-menu>
-            <v-text-field v-model='tx.notes' label='Notes' />
-          </template>
-          <template v-slot:col4>
-            <v-btn @click='add'>Add</v-btn>
-            <v-btn @click='showAdd = false'>Cancel</v-btn>
-          </template>
-        </row>
-        <txitem :class='{odd: index % 2 == 1}' v-for='(item, index) in list' :item='item' :key='item._id'/>
-      </v-flex>
-    </v-layout>
+    <v-data-table
+      :headers='headers'
+      :items='list'
+      disable-pagination
+      fixed-header
+      hide-default-footer
+    >
+      <template v-slot:item.date="{ item }">
+        <span>{{date(item.date)}}</span>
+      </template>
+      <template v-slot:item.total="{ item }">
+        <span>{{float(item.total)}}</span>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
 <script lang='coffee'>
 eventBus = require('./eventBus').default
+format = require('./format').default
 {Portfolio} = require('./model').default
 client = require('./mqtt').default
 {required, numeric, decimal} = require 'vuelidate/lib/validators'
@@ -76,6 +34,18 @@ export default
   props:
     tab: String
   data: ->
+    headers: [
+      { text: 'Name', value: 'name' }
+      { text: 'Symbol', value: 'symbol' }
+      { text: 'Quantity', value: 'quantity' }
+      { text: 'Type', value: 'type' }
+      { text: 'Price', value: 'price' }
+      { text: 'Date', value: 'date' }
+      { text: 'Tags', value: 'tags' }
+      { text: 'Notes', value: 'notes' }
+      { text: 'Total', value: 'total' }
+      { text: 'P&L', value: 'diffTotal' }
+    ] 
     list: []
     tags: []
     sort:
@@ -95,6 +65,8 @@ export default
     'tx.quantity': {required, numeric}
     'tx.price': {required, decimal}
   methods:
+    date: format.date
+    float: format.float
     add: ->
       @tx.quantity = parseInt @tx.quantity
       @tx.price = parseFloat @tx.price
