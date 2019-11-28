af368f0f049fe617de7f8f99d7df73ba57ec7973
root <twhtanghk@gmail.com>
2019-11-28 21:05:01 +0800

    add touchless to disable swipe

--- frontend/src/portfolio.vue
+++ frontend/src/portfolio.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-tabs fixed-tabs color='cyan' slider-color='yellow' @change='change'>
-    <v-tab>
-      Hold
-    </v-tab>
-    <v-tab>
-      TX
-    </v-tab> 
-    <v-tab-item>
-      <hold :tab='tab'/>
-    </v-tab-item>
-    <v-tab-item>
-      <tx :tab='tab'/>
-    </v-tab-item>
-  </v-tabs>
+  <div>
+    <v-tabs fixed-tabs color='cyan' slider-color='yellow' @change='change' v-model='index'>
+      <v-tab>
+        Hold
+      </v-tab>
+      <v-tab>
+        TX
+      </v-tab> 
+    </v-tabs>
+    <v-tabs-items touchless v-model='index'>
+      <v-tab-item>
+        <hold :tab='tab'/>
+      </v-tab-item>
+      <v-tab-item>
+        <tx :tab='tab'/>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
 </template>
 
 <script lang='coffee'>
@@ -24,6 +28,7 @@ export default
     hold: require('./hold').default
   data: ->
     tab: 'hold'
+    index: 0
   methods:
     change: (tab) ->
       @tab = ['hold', 'tx'][tab]
