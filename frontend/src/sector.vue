c5925a8f3a297838a49c433a5888796032eb095f
root <twhtanghk@gmail.com>
2020-01-09 13:22:09 +0800

    show peers stock name

--- frontend/src/sector.vue
+++ frontend/src/sector.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
-    <v-row v-for='sector in sectors'>
+    <v-row v-for='(sector, i) in sectors'>
       <v-col cols='12'>
-        <a :href='sector' target='_blank'>{{sector}}</a>:
+        <a :href='sector' target='_blank'>{{names[i]}}</a>
         <ad-chart :sector='sector'/>
       </v-col>
     </v-row>
@@ -10,6 +10,8 @@
 </template>
 
 <script lang='coffee'>
+Promise = require 'bluebird'
+{service} = require 'hkex'
 {Sector} = require('./model').default
 
 export default
@@ -17,6 +19,16 @@ export default
     adChart: require('./adChart').default
   data: ->
     sectors: []
+    names: []
+  methods:
+    name: (url) ->
+      code = url.match /.*=([0-9]*)/
+      ret = url
+      if code?
+        ret = await service.name code[1]
+      return ret
   mounted: ->
     @sectors = await Sector.list()
+    @names = await Promise.map @sectors, (s) =>
+      await @name s
 </script>
